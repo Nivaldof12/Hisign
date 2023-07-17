d0524e01ea96f1680f914276d084e7898db9f90c
Nivaldo <Nivaldo@host.docker.internal>
2023-07-17 09:30:49 -0300

    Primeiro Commit

---
+++ demo/src/main/java/com/example/demo/controller/repository/CandidatoRepository.java
@@ -0,0 +1,5 @@
+package com.example.demo.controller.repository;
+
+public class CandidatoRepository {
+
+}
---
+++ demo/src/main/java/com/example/demo/controller/service/CandidatoService.java
@@ -0,0 +1,5 @@
+package com.example.demo.controller.service;
+
+public class CandidatoService {
+
+}
