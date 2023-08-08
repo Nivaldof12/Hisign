c1f1dc96d4f589656b6c7865972a8cfb233098b2
Nivaldo <nivaldopvp@gmail.com>
2023-08-08 09:33:14 -0300

    Mudança no método alterar

--- demo/src/main/java/com/example/demo/controller/HorarioController.java
+++ demo/src/main/java/com/example/demo/controller/HorarioController.java
@@ -37,6 +37,31 @@ public class HorarioController {
 		if (horarioExistente != null) {
 			// Atualiza os atributos do horário existente com os valores do horário alterado
 			horarioExistente.setNomecompleto(horarioAlterado.getNomecompleto());
+			// Alterar Segunda
+			horarioExistente.setEntrada_Segunda(horarioAlterado.getEntrada_Segunda());
+			horarioExistente.setIntervalo_Segunda(horarioAlterado.getIntervalo_Segunda());
+			horarioExistente.setSaida_Segunda(horarioAlterado.getSaida_Segunda());
+			// Alterar Terça
+			horarioExistente.setEntrada_Terca(horarioAlterado.getEntrada_Terca());
+			horarioExistente.setIntervalo_Terca(horarioAlterado.getIntervalo_Terca());
+			horarioExistente.setSaida_Terca(horarioAlterado.getSaida_Terca());
+			// Alterar Quarta
+			horarioExistente.setEntrada_Quarta(horarioAlterado.getEntrada_Quarta());
+			horarioExistente.setIntervalo_Quarta(horarioAlterado.getIntervalo_Quarta());
+			horarioExistente.setSaida_Quarta(horarioAlterado.getSaida_Quarta());
+			// Alterar Quinta
+			horarioExistente.setEntrada_Quinta(horarioAlterado.getEntrada_Quinta());
+			horarioExistente.setIntervalo_Quinta(horarioAlterado.getIntervalo_Quinta());
+			horarioExistente.setSaida_Quinta(horarioAlterado.getSaida_Quinta());
+			// Alterar Sexta
+			horarioExistente.setEntrada_Sexta(horarioAlterado.getEntrada_Sexta());
+			horarioExistente.setIntervalo_Sexta(horarioAlterado.getIntervalo_Sexta());
+			horarioExistente.setSaida_Sexta(horarioAlterado.getSaida_Sexta());
+			// Alterar Sabado
+			horarioExistente.setEntrada_Sabado(horarioAlterado.getEntrada_Sabado());
+			horarioExistente.setIntervalo_Sabado(horarioAlterado.getIntervalo_Sabado());
+			horarioExistente.setSaida_Sabado(horarioAlterado.getSaida_Sabado());
+			// Alterar
 			horarioExistente.setObservacao(horarioAlterado.getObservacao());
 			horarioExistente.setTurno(horarioAlterado.getTurno());
 			horarioExistente.setEmpresa(horarioAlterado.getEmpresa());
