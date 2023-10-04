7f950a262e4189d63ac3bfc73db72b1445815c5a
Rodrigo Sarmento <rodrigogomessarmento@gmail.com>
2023-10-04 09:35:03 -0300

    refactor: alteração do size do horario

--- demo/src/main/java/com/example/demo/domain/Horario.java
+++ demo/src/main/java/com/example/demo/domain/Horario.java
@@ -28,90 +28,90 @@ public class Horario {
 	private String nomecompleto;
 
 	@Column(name = "entrada_Segunda")
-	@Size(min = 0, max = 5, message = "Entrada da Segunda-Feira inválida!")
+	@Size(min = 0, max = 20, message = "Entrada da Segunda-Feira inválida!")
 	private String entrada_Segunda;
 
 	@Column(name = "intervalo_Segunda")
-	@Size(min = 0, max = 5, message = "Intervalo da Segunda-Feira inválido!")
+	@Size(min = 0, max = 20, message = "Intervalo da Segunda-Feira inválido!")
 	private String intervalo_Segunda;
 
 	@Column(name = "saida_Segunda")
-	@Size(min = 0, max = 5, message = "Saída da Segunda-Feira inválida!")
+	@Size(min = 0, max = 20, message = "Saída da Segunda-Feira inválida!")
 	private String saida_Segunda;
 	
 	@Column(name = "horas_dia_Segunda")
 	private Double horas_dia_Segunda;
 
 	@Column(name = "entrada_Terca")
-	@Size(min = 0, max = 5, message = "Entrada da Terça-Feira inválido!")
+	@Size(min = 0, max = 20, message = "Entrada da Terça-Feira inválido!")
 	private String entrada_Terca;
 
 	@Column(name = "intervalo_Terca")
-	@Size(min = 0, max = 5, message = "Intervalo da Terça-Feira inválido!")
+	@Size(min = 0, max = 20, message = "Intervalo da Terça-Feira inválido!")
 	private String intervalo_Terca;
 
 	@Column(name = "saida_Terca")
-	@Size(min = 0, max = 5, message = "Saída da Terça-Feira inválida!")
+	@Size(min = 0, max = 20, message = "Saída da Terça-Feira inválida!")
 	private String saida_Terca;
 	
 	@Column(name = "horas_dia_Terca")
 	private Double horas_dia_Terca;
 
 	@Column(name = "entrada_Quarta")
-	@Size(min = 0, max = 5, message = "Entrada da Quarta-Feira inválida!")
+	@Size(min = 0, max = 20, message = "Entrada da Quarta-Feira inválida!")
 	private String entrada_Quarta;
 
 	@Column(name = "intervalo_Quarta")
-	@Size(min = 0, max = 5, message = "Intervalo da Quarta-feira inválido!")
+	@Size(min = 0, max = 20, message = "Intervalo da Quarta-feira inválido!")
 	private String intervalo_Quarta;
 
 	@Column(name = "saida_Quarta")
-	@Size(min = 0, max = 5, message = "Saída da Quarta-feira inválido!")
+	@Size(min = 0, max = 20, message = "Saída da Quarta-feira inválido!")
 	private String saida_Quarta;
 	
 	@Column(name = "horas_dia_Quarta")
 	private Double horas_dia_Quarta;
 
 	@Column(name = "entrada_Quinta")
-	@Size(min = 0, max = 5, message = "Entrada da Quinta-feira inválido!")
+	@Size(min = 0, max = 20, message = "Entrada da Quinta-feira inválido!")
 	private String entrada_Quinta;
 
 	@Column(name = "intervalo_Quinta")
-	@Size(min = 0, max = 5, message = "Intervalo da Quinta-feira inválido!")
+	@Size(min = 0, max = 20, message = "Intervalo da Quinta-feira inválido!")
 	private String intervalo_Quinta;
 
 	@Column(name = "saida_Quinta")
-	@Size(min = 0, max = 5, message = "Saída da Quinta-feira inválido!")
+	@Size(min = 0, max = 20, message = "Saída da Quinta-feira inválido!")
 	private String saida_Quinta;
 	
 	@Column(name = "horas_dia_Quinta")
 	private Double horas_dia_Quinta;
 
 	@Column(name = "entrada_Sexta")
-	@Size(min = 0, max = 5, message = "Entrada da Sexta-feira inválida!")
+	@Size(min = 0, max = 20, message = "Entrada da Sexta-feira inválida!")
 	private String entrada_Sexta;
 
 	@Column(name = "intervalo_Sexta")
-	@Size(min = 0, max = 5, message = "Intervalo da Sexta-feira inválido!")
+	@Size(min = 0, max = 20, message = "Intervalo da Sexta-feira inválido!")
 	private String intervalo_Sexta;
 
 	@Column(name = "saida_Sexta")
-	@Size(min = 0, max = 5, message = "Saída da Sexta-feira inválido!")
+	@Size(min = 0, max = 20, message = "Saída da Sexta-feira inválido!")
 	private String saida_Sexta;
 	
 	@Column(name = "horas_dia_Sexta")
 	private Double horas_dia_Sexta;
 
 	@Column(name = "entrada_Sabado")
-	@Size(min = 0, max = 5, message = "Entrada do sábado inválido!")
+	@Size(min = 0, max = 20, message = "Entrada do sábado inválido!")
 	private String entrada_Sabado;
 
 	@Column(name = "intervalo_Sabado")
-	@Size(min = 0, max = 5, message = "Intervalo do sábado inválido!")
+	@Size(min = 0, max = 20, message = "Intervalo do sábado inválido!")
 	private String intervalo_Sabado;
 
 	@Column(name = "saida_Sabado")
-	@Size(min = 0, max = 5, message = "Saída do sábado inválido!")
+	@Size(min = 0, max = 20, message = "Saída do sábado inválido!")
 	private String saida_Sabado;
 	
 	@Column(name = "horas_dia_Sabado")
