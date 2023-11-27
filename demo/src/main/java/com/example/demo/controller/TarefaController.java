98f272543398abc38425e40a9b224b629a2243fd
Rodrigo Sarmento <rodrigogomessarmento@gmail.com>
2023-11-27 11:09:17 -0300

    feat: add end-points de excluir e alterar

--- demo/src/main/java/com/example/demo/controller/TarefaController.java
+++ demo/src/main/java/com/example/demo/controller/TarefaController.java
@@ -1,6 +1,7 @@
 package com.example.demo.controller;
 
 import com.example.demo.domain.Tarefa;
+import com.example.demo.domain.Trilha;
 import com.example.demo.service.TarefaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -22,5 +23,31 @@ public class TarefaController {
     @GetMapping(value = "/lista")
     public ResponseEntity<Object> telaLista() {
         return ResponseEntity.ok(tarefaService.obterLista());
+    }
+
+    @DeleteMapping(value = "/{id}/excluir")
+    public ResponseEntity<String> excluir(@PathVariable Integer id) {
+        tarefaService.excluirTarefaPorId(id);
+        return ResponseEntity.ok("Tarefa excluída com sucesso!");
+    }
+
+    @PutMapping(value = "/{id}/alterar")
+    public ResponseEntity<String> alterar(@PathVariable Integer id, @Validated @RequestBody Tarefa tarefaAlterada) {
+        Tarefa tarefaexistente = tarefaService.obterTarefaPorId(id);
+        if (tarefaexistente != null) {
+            // Atualiza os atributos da tarefa existente com os valores da tarefa alterada
+            tarefaexistente.setData(tarefaAlterada.getData());
+            tarefaexistente.setTitulo(tarefaAlterada.getTitulo());
+            tarefaexistente.setAnotacao(tarefaAlterada.getAnotacao());
+            tarefaexistente.setHorario_inicio(tarefaAlterada.getHorario_inicio());
+            tarefaexistente.setHorario_fim(tarefaAlterada.getHorario_fim());
+            tarefaexistente.setLink_reuniao(tarefaAlterada.getLink_reuniao());
+
+            // Salva as alterações no banco de dados
+            tarefaService.incluir(tarefaexistente);
+            return ResponseEntity.ok("Tarefa alterada com sucesso!");
+        } else {
+            return ResponseEntity.notFound().build();
+        }
     }
 }
