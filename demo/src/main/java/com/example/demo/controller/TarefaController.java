ffeb48ffc704d0e4ea04f6b95a6c6a7de026cbb9
Rodrigo Sarmento <rodrigogomessarmento@gmail.com>
2023-11-30 11:42:29 -0300

    fix: add value na rota

--- demo/src/main/java/com/example/demo/controller/TarefaController.java
+++ demo/src/main/java/com/example/demo/controller/TarefaController.java
@@ -8,7 +8,7 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/tarefa")
+@RequestMapping(value = "/tarefa")
 public class TarefaController {
     @Autowired
     private TarefaService tarefaService;
