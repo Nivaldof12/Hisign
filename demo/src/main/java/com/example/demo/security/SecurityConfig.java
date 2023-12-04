f638c2bc7fa758b07fc216b76c69944f7340d80b
Rodrigo Sarmento <rodrigogomessarmento@gmail.com>
2023-12-04 15:01:34 -0300

    feat: add configuração do spring security

---
+++ demo/src/main/java/com/example/demo/security/SecurityConfig.java
@@ -0,0 +1,25 @@
+package com.example.demo.security;
+
+import feign.Request;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .csrf(csrf -> csrf.disable())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeRequests(authorize -> authorize
+                        .antMatchers(HttpMethod.POST, "/tarefa").hasRole("ADMIN")
+                        .anyRequest().authenticated())
+                .build();
+    }
+}
