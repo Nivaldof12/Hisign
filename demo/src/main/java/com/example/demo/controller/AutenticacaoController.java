52532dc34771e645fd9fd321cfe7dff7b9ebc656
Rodrigo Sarmento <rodrigogomessarmento@gmail.com>
2023-12-04 15:18:18 -0300

    feat: add cryptogrofia de senhas e controller de autenticacao

--- demo/src/main/java/com/example/demo/controller/AutenticacaoController.java
+++ demo/src/main/java/com/example/demo/controller/AutenticacaoController.java
@@ -1,7 +1,12 @@
 package com.example.demo.controller;
 
 import com.example.demo.domain.usuario.AutenticacaoDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.neo4j.Neo4jProperties;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,6 +17,13 @@ import javax.validation.Valid;
 @RestController
 @RequestMapping("auth")
 public class AutenticacaoController {
+    @Autowired
+    private AuthenticationManager authenticationManager;
     @PostMapping("/login")
-    public ResponseEntity login(@RequestBody @Valid AutenticacaoDTO data)
+    public ResponseEntity login(@RequestBody @Valid AutenticacaoDTO data){
+        var usernamePassword = new UsernamePasswordAuthenticationToken(data.email(), data.senha());
+        var auth = this.authenticationManager.authenticate(usernamePassword);
+
+        return ResponseEntity.ok().build();
+    }
 }
--- demo/src/main/java/com/example/demo/security/SecurityConfig.java
+++ demo/src/main/java/com/example/demo/security/SecurityConfig.java
@@ -4,9 +4,13 @@ import feign.Request;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -21,5 +25,13 @@ public class SecurityConfig {
                         .antMatchers(HttpMethod.POST, "/tarefa").hasRole("ADMIN")
                         .anyRequest().authenticated())
                 .build();
+    }
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception{
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
     }
 }
