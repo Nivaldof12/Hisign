c03ebad995f605d5da16f3ea7a1bf4f82e0ba08b
Rodrigo Sarmento <rodrigogomessarmento@gmail.com>
2023-12-04 11:09:57 -0300

    feat: criação do repository de usuario

---
+++ demo/src/main/java/com/example/demo/repository/UsuarioRepository.java
@@ -0,0 +1,9 @@
+package com.example.demo.repository;
+
+import com.example.demo.domain.usuario.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
+    UserDetails findByEmail(String email);
+}
