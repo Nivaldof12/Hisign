76b0b958cc044f6794646f2c53afb0dfe027d044
Rodrigo Sarmento <rodrigogomessarmento@gmail.com>
2023-12-04 15:05:59 -0300

    feat: add autenticacaoDTO

--- demo/src/main/java/com/example/demo/controller/AutenticacaoController.java
+++ demo/src/main/java/com/example/demo/controller/AutenticacaoController.java
@@ -1,7 +1,17 @@
 package com.example.demo.controller;
 
+import com.example.demo.domain.usuario.AutenticacaoDTO;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Valid;
+
 @RestController
+@RequestMapping("auth")
 public class AutenticacaoController {
+    @PostMapping("/login")
+    public ResponseEntity login(@RequestBody @Valid AutenticacaoDTO data)
 }
