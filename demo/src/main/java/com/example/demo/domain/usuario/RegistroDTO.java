9703bc8a7d641aa20a8ac767b74c4c2e82d2ed24
Rodrigo Sarmento <rodrigogomessarmento@gmail.com>
2023-12-07 11:57:50 -0300

    feat: add validações no registroDTO

--- demo/src/main/java/com/example/demo/domain/usuario/RegistroDTO.java
+++ demo/src/main/java/com/example/demo/domain/usuario/RegistroDTO.java
@@ -1,4 +1,18 @@
 package com.example.demo.domain.usuario;
 
-public record RegistroDTO(String email, String senha, String nome, String regra) {
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+public record RegistroDTO(
+        @Email(message = "Email inválido!")
+        @NotBlank(message = "Email não pode ficar em branco!")
+        String email,
+        @NotBlank(message = "Senha não pode ficar em branco!")
+        @Size(min = 5, max = 500, message = "Senha inválida!(Deve conter no mínimo 5 caracteres)")
+        String senha,
+        @NotBlank(message = "Nome não pode ficar em branco!")
+        @Size(min = 3, max = 50, message = "Nome inválido!(Deve conter no mínimo 3 caracteres e no máximo 50)")
+        String nome,
+        String regra) {
 }
