97601b08353f497f03b59e6a6191daeb30474267
Rodrigo Sarmento <rodrigogomessarmento@gmail.com>
2023-12-07 09:36:44 -0300

    feat: add geração de token

---
+++ demo/src/main/java/com/example/demo/security/TokenService.java
@@ -0,0 +1,38 @@
+package com.example.demo.security;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import com.example.demo.domain.usuario.Usuario;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
+@Service
+public class TokenService {
+    @Value("${api.security.token.secret}")
+    private String segredo;
+
+    public String geradorToken(Usuario usuario){
+        try{
+            Algorithm algorithm = Algorithm.HMAC256(segredo);
+            String token = JWT.create()
+                    .withIssuer("auth-hisig")
+                    .withSubject(usuario.getEmail())
+                    .withExpiresAt(genExpiracaoToken())
+                    .sign(algorithm);
+            return token;
+        }catch (JWTCreationException exception){
+            throw new RuntimeException("Errro na geração do token", exception);
+        }
+
+    }
+
+    private Instant genExpiracaoToken(){
+        //tempo de expiração do token: 5 horas
+        return LocalDateTime.now().plusHours(5).toInstant(ZoneOffset.of("-03:00"));
+    }
+}
